89bdd33d62ae62f43865777d9e4b42f3ce482d42
Walter Fabri <walterfabri18@gmail.com>
2023-04-26 15:48:05 -0300

    introduzindo logica de decremento de tempo

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,30 @@ class Cronometro {
 
     iniciaContagem() {
         console.log('Chamou o método');
-        relogioVisor.innerHTML = '';
+        
+        let minutos = 25;
+        let segundos = 0;
+
+        let myInterval = setInterval(function() {
+            
+            /* Lógica para os minutos e segundos */
+            if (segundos == 0) {
+                minutos = minutos - 1;
+                segundos = 60;
+                segundos = segundos - 1;
+            } else {
+                segundos = segundos - 1;
+            }
+
+
+            /* Acabou o tempo */
+            if (minutos == 0 && segundos == 0) {
+                clearInterval(myInterval);
+            }
+
+
+            mostraHora(minutos, segundos);
+        }, 1000)
     }
 
     iniciaFolgaCurta() {
@@ -22,9 +45,38 @@ class Cronometro {
 
 }
 
+/* Instâncias */
 const cronometro = new Cronometro();
+const data = new Date();
+
+/* Elementos HTML selecionados */
 
 let btnStartTimer = document.querySelector('#btn-start-timer');
 let relogioVisor = document.querySelector('#timer');
+let divBtnTimer = document.querySelector('#div-btn-timer');
+
+
+
+/* Captura de eventos */
 
 btnStartTimer.addEventListener('click', () => cronometro.iniciaContagem());
+btnPausaTimer.addEventListener('click', () => cronometro.pausa());
+
+
+
+/* Funções */
+
+function adicionaZero(numero) {
+    if (numero < 10) {
+        numero = "0" + numero;
+    }
+    return numero;
+}
+
+
+function mostraHora(minutos, segundos) {
+    data.setMinutes(minutos);
+    data.setSeconds(segundos);
+    let cronometro = `${adicionaZero(data.getMinutes())}:${adicionaZero(data.getSeconds())}`; 
+    relogioVisor.innerHTML = cronometro;
+}
